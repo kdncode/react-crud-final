70f3c6462d7e34389702d0e290752cacd300f2ee
kdncode <kdncode@gmail.com>
2018-05-09 23:56:49 -0500

    updated key index

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,7 +5,7 @@ class TaskList extends Component {
   render() {
     var { tasks } = this.props;
     var elementTasks = tasks.map((task, index) => {
-        return <TaskItem />
+        return <TaskItem key={task.id} index />
     })
 
     return (
