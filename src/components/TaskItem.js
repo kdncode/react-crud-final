2a3336667de37da998c59e67042e09b496dacf4f
kdncode <kdncode@gmail.com>
2018-05-10 23:55:14 -0500

    updated

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -18,8 +18,8 @@ class TaskItem extends Component {
             <td className="text-center">
                 <button type="button" className="btn btn-warning">
                     <span className="fa fa-pencil mr-5"></span>Edit
-                </button>
-                &nbsp;
+                </button> &nbsp;
+
                 <button type="button" className="btn btn-danger">
                     <span className="fa fa-trash mr-5"></span>Delete
                 </button>
